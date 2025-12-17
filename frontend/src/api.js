294a6addc6b2173177c14869c69db0b72a82e1f7
alokrj01 <ranjanalok2468@gmail.com>
2025-12-17 23:19:53 +0530

    Connect frontend with HF backend API

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
+const BASE_URL = import.meta.env.VITE_API_URL || "https://alokrj-plant-disease-backend.hf.space";
 
 export const api = axios.create({
   baseURL: BASE_URL,
